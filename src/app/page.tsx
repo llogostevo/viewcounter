96d07157cf1946c8b4de9c461c7c4bc488958e0f
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-08-17 14:38:30 +0100

    fetch api created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+// create an async function
+export default async function Home() {
+  // make a constant to fetch the result from the api
+  // first argument is the api to fetch from, the second argument is an option. 
+  // next object revalidate 5 will revalidate the fetch call every 5 seconds
+  // nb. this is not making a call every 5 seconds, if it has been more than 5 seconds since its last attempt at revalidating the data, then revalidate
+  const res = await fetch("https://api.github.com/repos/llogostevo/viewcounter", {next:{revalidate: 5}})
+  
+  // get the data and store into a joson response
+  const data = await res.json()
+  console.log(data.stargazers_count)
   return (
     <main >
-     0
+     <p>{data.stargazers_count}</p>
     </main>
   )
 }
