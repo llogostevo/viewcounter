0a1abb16710d84b2c4ab2e52dd93f3dc2d4b23d7
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-08-17 15:24:23 +0100

    linked kv store and page counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,28 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import {kv} from "@vercel/kv"
 
 // create an async function
 export default async function Home() {
+
+  // ## Favourites on Github ##
   // make a constant to fetch the result from the api
   // first argument is the api to fetch from, the second argument is an option. 
-  // next object revalidate 5 will revalidate the fetch call every 5 seconds
-  // nb. this is not making a call every 5 seconds, if it has been more than 5 seconds since its last attempt at revalidating the data, then revalidate
   const res = await fetch("https://api.github.com/repos/llogostevo/viewcounter", {next:{revalidate: 5}})
   
   // get the data and store into a joson response
-  const data = await res.json()
+  const data = await res.json() 
   console.log(data.stargazers_count)
+  // ## github ##
+
+// ## HOW MANY TIMES PAGE HAS BEEN LIKED ## 
+const pageViews = await kv.incr("views")
+ 
   return (
     <main >
-     <p>⭐{data.stargazers_count}</p>
+     <p>{pageViews}</p>
+     <span>⭐{data.stargazers_count}</span>
     </main>
   )
 }
+
